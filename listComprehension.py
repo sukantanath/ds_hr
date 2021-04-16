be28225e15c19a94b8663d653249dfd64ace3a4b
Sukanta Nath <nath.sukanta@gmail.com>
2021-04-16 09:24:00 +0530

    Create listComprehension.py

---
+++ listComprehension.py
@@ -0,0 +1,12 @@
+ #You are given three integers x,y,z representing the dimensions of a cuboid along with an integer n. 
+ #Print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum of (i+j+k) is not equal to n. 
+# Here, 0< i < x, 0 < j < y ,0 < k < z. Please use list comprehensions rather than multiple loops.
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    res_list = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k) != n ]
+    print(res_list)
